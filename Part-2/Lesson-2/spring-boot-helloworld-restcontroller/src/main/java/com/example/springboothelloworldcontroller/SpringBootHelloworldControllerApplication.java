26f3313d05e1e065a4a8bfbd2ac7bb6f629c6ee7
Mattw46 <mattw76@live.com.au>
2022-08-08 21:36:05 +1000

    Rest Controller

---
+++ Part-2/Lesson-2/spring-boot-helloworld-restcontroller/src/main/java/com/example/springboothelloworldcontroller/SpringBootHelloworldControllerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboothelloworldcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHelloworldControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHelloworldControllerApplication.class, args);
+	}
+
+}
